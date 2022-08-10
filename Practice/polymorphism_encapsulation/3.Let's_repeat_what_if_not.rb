d486f3d32dcc011ff9e479b3c7ba74c0341eb97e
Jimmy Rojas <90005667+Jimmy123r@users.noreply.github.com>
2022-08-10 15:43:35 -0300

    Create 3.Let's_repeat_what_if_not.rb
    module Gardener
      def self.care!(plant)
        if plant.needs_water?
          3.times { self.water! plant }
        else
          self.remove_bugs! plant
        end
      end
    end
    And what is that times?
    
    It is a message that numbers understand that is used to execute a piece of code several times. In this case we would water the plant received as an argument 3 times.
    
    Now that we know about the existence of times and we have seen how to do else...
    
    Modify the solution so that if Pepita is not weak, she flies in circles 3 times.

---
+++ Practice/polymorphism_encapsulation/3.Let's_repeat_what_if_not.rb
@@ -0,0 +1,31 @@
+module Pepita
+  @energia = 1000
+
+  def self.energia
+    @energia 
+  end
+
+  def self.volar_en_circulos!
+    @energia -= 10
+  end
+  
+  def self.comer_alpiste!(gramos)
+    @energia += gramos * 15
+  end  
+
+  def self.debil?
+    return @energia < 100
+  end
+  
+  def self.feliz?
+    return @energia > 1000
+  end
+  def self.hacer_lo_que_quiera!
+    if self.debil?
+      self.comer_alpiste!(10)
+    else 
+      3.times {volar_en_circulos! }
+    end 
+  end
+end
+
