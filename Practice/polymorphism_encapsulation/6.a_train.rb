124d2f89d275539e06fc67d97d04ae094f021b69
Jimmy Rojas <90005667+Jimmy123r@users.noreply.github.com>
2022-08-11 15:34:27 -0300

    Create 6.a_train.rb
    Our birds want to perform at the next Olympics, and for that they need to exercise a little.
    
    To help us in this task we got Pachorra, a former soccer coach who now works with birds. He designed a special routine consisting of the following:
    
    Fly in circles 10 times.
    Eat a handful of 30 grams of birdseed.
    Fly in circles 5 times.
    As a reward, let the bird do what it wants.
    Create Pachorra, the bird trainer, and when you receive the message train_bird! have Pepita perform her routine (yes, you can only enter Pepita , but we'll fix that soon).

---
+++ Practice/polymorphism_encapsulation/6.a_train.rb
@@ -0,0 +1,8 @@
+module Pachorra
+  def self.entrenar_ave!
+    10.times{Pepita.volar_en_circulos!}
+    Pepita.comer_alpiste! 30
+    5.times{Pepita.volar_en_circulos!}
+    Pepita.hacer_lo_que_quiera!
+  end
+end 
