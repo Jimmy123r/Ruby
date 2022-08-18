6a4378e9a4a8a4c74dc8dfccac74d3ef14218c1a
Jimmy Rojas <90005667+Jimmy123r@users.noreply.github.com>
2022-08-18 16:52:44 -0300

    Create  super.rb

---
+++ Practice/inheritance/super.rb
@@ -0,0 +1,20 @@
+class Colectivo < MedioDeTransporte
+  def initialize
+    @combustible = 100
+    @pasajeros = 0
+  end
+  def entran? pasajeros
+    true
+  end
+  def  maximo_personas
+    @pasajeros
+  end
+  def recorrer!(km)
+    @combustible-=km * 2
+  end
+  def cargar_combustible!(combustible)
+       super
+      @pasajeros = 0
+       
+    end
+end
